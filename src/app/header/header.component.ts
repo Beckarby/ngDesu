ec8943d2f83fa82cf1ba5d2d157b487bbdc90c7f
Rebecca Bracho <rebeccabracho05@gmail.com>
2026-06-27 02:10:42 -0400

    feat: profile page

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: 'header.component.html',
+  styleUrls: ['header.component.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle],
+})
+export class HeaderComponent {
+  @Input() subtitle = '';
+}
--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { RouterLink } from '@angular/router';
+import { IonContent, IonIcon, IonList, IonItem, IonLabel, IonAvatar } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { personCircleOutline, chatbubbleOutline, chevronForward, personOutline, logOutOutline, starOutline, tvOutline } from 'ionicons/icons';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-profile',
   templateUrl: 'profile.page.html',
   styleUrls: ['profile.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [RouterLink, IonContent, IonIcon, IonList, IonItem, IonLabel, IonAvatar, HeaderComponent],
 })
 export class profilePage {
-  constructor() {}
+  constructor() {
+    addIcons({
+      'person-circle-outline': personCircleOutline,
+      'tv-outline': tvOutline,
+      'chatbubble-outline': chatbubbleOutline,
+      'chevron-forward': chevronForward,
+      'person-outline': personOutline,
+      'log-out-outline': logOutOutline,
+      'star-outline': starOutline,
+    });
+  }
 }
