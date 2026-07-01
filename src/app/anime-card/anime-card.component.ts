89ae6a43b5fead909430739a0ad3981db82a47f2
Rebecca Bracho <rebeccabracho05@gmail.com>
2026-07-01 00:40:35 -0400

    feat: add anime card component with template and styles

---
+++ src/app/anime-card/anime-card.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-anime-card',
+  templateUrl: './anime-card.component.html',
+  styleUrls: ['./anime-card.component.scss'],
+})
+export class AnimeCardComponent {
+  @Input() anime?: { id: number; name: string; image?: string };
+}
