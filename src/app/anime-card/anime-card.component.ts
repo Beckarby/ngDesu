0e79dc8da1059f5d94967a3c0312358e20bcd7a1
Rebecca Bracho <rebeccabracho05@gmail.com>
2026-07-08 01:16:04 -0400

    feat: enable navigation to anime detail page from anime card component

--- src/app/anime-card/anime-card.component.ts
+++ src/app/anime-card/anime-card.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-anime-card',
   templateUrl: './anime-card.component.html',
   styleUrls: ['./anime-card.component.scss'],
+  imports: [RouterLink],
 })
 export class AnimeCardComponent {
   @Input() anime?: { id: number; name: string; image?: string; userScore?: number; criticScore?: number };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,10 @@ export const routes: Routes = [
     loadComponent: () => import('./login/login.page').then((m) => m.loginPage),
   },
   {
+    path: 'anime/:id',
+    loadComponent: () => import('./anime-detail/anime-detail.page').then((m) => m.AnimeDetailPage),
+  },
+  {
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
