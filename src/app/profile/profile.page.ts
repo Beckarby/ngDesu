1f3c8e4cb929eba72c9bded88dac3b907941ff89
Rebecca Bracho <rebeccabracho05@gmail.com>
2026-07-09 01:29:55 -0400

    feat: add avatar image support and update sign-out functionality in profile page

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { IonContent, IonIcon, IonList, IonItem, IonLabel, IonAvatar } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { IonContent, IonIcon, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { personCircleOutline, chatbubbleOutline, chevronForward, personOutline, logOutOutline, starOutline, tvOutline } from 'ionicons/icons';
 import { HeaderComponent } from '../header/header.component';
+import { ProfileStore } from '../store/profile-store.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: 'profile.page.html',
   styleUrls: ['profile.page.scss'],
-  imports: [RouterLink, IonContent, IonIcon, IonList, IonItem, IonLabel, IonAvatar, HeaderComponent],
+  imports: [RouterLink, IonContent, IonIcon, IonList, IonItem, IonLabel, HeaderComponent],
 })
 export class profilePage {
+  private router = inject(Router);
+  protected profileStore = inject(ProfileStore);
+
+  signOut() {
+    this.profileStore.currentAvatar.set(null);
+    this.profileStore.nickname.set(null);
+    this.router.navigateByUrl('/login');
+  }
+
   constructor() {
     addIcons({
       'person-circle-outline': personCircleOutline,
